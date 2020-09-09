6376b60e663da5f7153f3b704b943e7cb8472aca
unknown <jungwookim37@hanmail.net>
2020-09-10 03:32:03 +0900

    liveness

---
+++ src/main/java/cnabookstore/ZombieController.java
@@ -0,0 +1,29 @@
+package cnabookstore;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ZombieController {
+
+    private boolean flag;
+
+    public ZombieController(){
+        flag = true;
+    }
+
+    @GetMapping({"/isHealthy"})
+    public void zombie2() throws Exception {
+        if (flag)
+            return;
+        else
+            throw new Exception("zombie.....");
+    }
+
+    @GetMapping({"/makeZombie"})
+    public void getStockInputs() {
+
+        flag = false;
+
+    }
+}
