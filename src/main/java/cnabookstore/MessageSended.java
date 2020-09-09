3eb4b079211819b30159ab6e23443f910a239329
unknown <jungwookim37@hanmail.net>
2020-09-09 23:54:51 +0900

    add kakaoMessage

--- src/main/java/cnabookstore/MessageSended.java
+++ src/main/java/cnabookstore/MessageSended.java
@@ -3,6 +3,34 @@ package cnabookstore;
 public class MessageSended extends AbstractEvent {
 
     private Long id;
+    private Long deliveryId;
+
+    public Long getDeliveryId() {
+        return deliveryId;
+    }
+
+    public void setDeliveryId(Long deliveryId) {
+        this.deliveryId = deliveryId;
+    }
+
+    public String getKakaoMessage() {
+        return kakaoMessage;
+    }
+
+    public void setKakaoMessage(String kakaoMessage) {
+        this.kakaoMessage = kakaoMessage;
+    }
+
+    public String getDeliveryStatus() {
+        return deliveryStatus;
+    }
+
+    public void setDeliveryStatus(String deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
+
+    private String kakaoMessage;
+    private String deliveryStatus;
 
     public MessageSended(){
         super();
